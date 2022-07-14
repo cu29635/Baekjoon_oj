f5ab900b5c952daf5ced4c9b31eff384b306e618
gyul <77824364+cu29635@users.noreply.github.com>
2022-07-15 01:36:27 +0900

    [Bronze II] Title: 피보나치 수, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.py
@@ -0,0 +1,16 @@
+def fibonacci(n):
+  d[0] = 0
+  d[1] = 1
+
+  if(n<2):
+    return d[n]
+
+  for i in range(2,n+1):
+    d[i] = d[i-2] + d[i-1]
+
+  return d[n]
+
+if __name__ == '__main__':
+  n = int(input())
+  d = [0 for i in range(100)]
+  print(fibonacci(n))
