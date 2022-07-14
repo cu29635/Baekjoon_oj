a242193c0b2d978adedc7ad8d24c87a0a2e4c7d2
gyul <77824364+cu29635@users.noreply.github.com>
2022-07-15 01:41:11 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 52 ms, Memory: 4900 KB -BaekjoonHub

---
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+
+    int k = 0;
+    int result[1000001] = {0,};
+    result[1] = 1;
+    for(int i = 2;i<1000001;i++) {
+        if(result[i]==0) {
+            for(int j=i*2;j<1000001;j=j+i) result[j] = 1;
+        }
+    }
+    scanf("%d", &k);
+    for(int j=0;j<k;j++){
+        int cnt=0;
+        int n;
+        scanf("%d", &n);
+        if(n==0) break;
+        for(int i = 2;i<n/2+1;i++){
+            if(result[i]==0&&result[n-i]==0){
+                cnt++;
+            }
+        }
+        printf("%d\n", cnt);
+    }
+    return 0;
+}
